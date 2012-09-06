ac180799474e24f71208a6d6f6898bf29046c8a8
gzip4 <gzip4ever@gmail.com>
2012-09-06 16:50:30 +0600

    loader compilation and linking stuff done

--- mkldr/main.cc
+++ mkldr/main.cc
@@ -1,4 +1,8 @@
 
-extern "C" void loader_main(void *x, void *y)
+extern "C" int printf(const char *, ...);
+
+extern "C" void
+loader_main(unsigned magic, unsigned mbi)
 {
+	printf("Hello: %p %p\n", magic, mbi);
 }
