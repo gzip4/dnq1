dd93957810dd2b4b7180876f7ff3c19fa6c90ae0
gzip4 <gzip4ever@gmail.com>
2012-09-17 10:46:43 +0600

    mkldr: fix types in mp.cc

--- mkldr/mp.cc
+++ mkldr/mp.cc
@@ -2,6 +2,7 @@
 // Search memory for MP description structures.
 // http://developer.intel.com/design/pentium/datashts/24201606.pdf
 
+#include <dnq/dnq.h>
 #include <config.h>
 #include "subc.h"
 
@@ -9,55 +10,50 @@
 
 // See MultiProcessor Specification Version 1.[14]
 
-typedef unsigned char  uchar;
-typedef unsigned short ushort;
-typedef unsigned int   uint;
-typedef unsigned long  ulong;
-
 
 struct mp {		// floating pointer
-	uchar signature[4];	// "_MP_"
+	uint8 signature[4];	// "_MP_"
 	void *physaddr;		// phys addr of MP config table
-	uchar length;		// 1
-	uchar specrev;		// [14]
-	uchar checksum;		// all bytes must add up to 0
-	uchar type;		// MP system config type
-	uchar imcrp;
-	uchar reserved[3];
+	uint8 length;		// 1
+	uint8 specrev;		// [14]
+	uint8 checksum;		// all bytes must add up to 0
+	uint8 type;		// MP system config type
+	uint8 imcrp;
+	uint8 reserved[3];
 };
 
 struct mpconf {		// configuration table header
-	uchar signature[4];	// "PCMP"
-	ushort length;		// total table length
-	uchar version;		// [14]
-	uchar checksum;		// all bytes must add up to 0
-	uchar product[20];	// product id
-	uint *oemtable;		// OEM table pointer
-	ushort oemlength;	// OEM table length
-	ushort entry;		// entry count
-	uint *lapicaddr;	// address of local APIC
-	ushort xlength;		// extended table length
-	uchar xchecksum;	// extended table checksum
-	uchar reserved;
+	uint8 signature[4];	// "PCMP"
+	uint16 length;		// total table length
+	uint8 version;		// [14]
+	uint8 checksum;		// all bytes must add up to 0
+	uint8 product[20];	// product id
+	uint32 *oemtable;	// OEM table pointer
+	uint16 oemlength;	// OEM table length
+	uint16 entry;		// entry count
+	uint32 *lapicaddr;	// address of local APIC
+	uint16 xlength;		// extended table length
+	uint8 xchecksum;	// extended table checksum
+	uint8 reserved;
 };
 
 struct mpproc {		// processor table entry
-	uchar type;		// entry type (0)
-	uchar apicid;		// local APIC id
-	uchar version;		// local APIC verison
-	uchar flags;		// CPU flags
+	uint8 type;		// entry type (0)
+	uint8 apicid;		// local APIC id
+	uint8 version;		// local APIC verison
+	uint8 flags;		// CPU flags
     #define MPBOOT 0x02		// This proc is the bootstrap processor.
-	uchar signature[4];	// CPU signature
-	uint feature;		// feature flags from CPUID instruction
-	uchar reserved[8];
+	uint8 signature[4];	// CPU signature
+	uint32 feature;		// feature flags from CPUID instruction
+	uint8 reserved[8];
 };
 
 struct mpioapic {	// I/O APIC table entry
-	uchar type;		// entry type (2)
-	uchar apicno;		// I/O APIC id
-	uchar version;		// I/O APIC version
-	uchar flags;		// I/O APIC flags
-	uint *addr;		// I/O APIC address
+	uint8 type;		// entry type (2)
+	uint8 apicno;		// I/O APIC id
+	uint8 version;		// I/O APIC version
+	uint8 flags;		// I/O APIC flags
+	uint32 *addr;		// I/O APIC address
 };
 
 // Table entry types
@@ -70,7 +66,7 @@ struct mpioapic {	// I/O APIC table entry
 
 
 
-static inline void outb(ushort port, uchar val)
+static inline void outb(uint16 port, uint8 val)
 {
 	__asm__ __volatile__ (
 		"outb %0, %1"
@@ -79,9 +75,9 @@ static inline void outb(ushort port, uchar val)
 	);
 }
 
-static inline uchar inb(ushort port)
+static inline uint8 inb(uint16 port)
 {
-	uchar ret;
+	uint8 ret;
 	__asm__ __volatile__ (
 		"inb %1, %0"
 		: "=a"(ret)
@@ -91,11 +87,11 @@ static inline uchar inb(ushort port)
 }
 
 
-static int memcmp(const void *v1, const void *v2, uint n)
+static int memcmp(const void *v1, const void *v2, uint32 n)
 {
-	const uchar *s1, *s2;
-	s1 = (const uchar *) v1;
-	s2 = (const uchar *) v2;
+	const uint8 *s1, *s2;
+	s1 = (const uint8 *) v1;
+	s2 = (const uint8 *) v2;
 	while (n-- > 0) {
 		if (*s1 != *s2)
 			return *s1 - *s2;
@@ -108,35 +104,35 @@ static int memcmp(const void *v1, const void *v2, uint n)
 
 struct cpu
 {
-	uchar id;
+	uint8 id;
 };
 
-extern volatile uint *lapic;
+extern volatile uint32 *lapic;
 struct cpu cpus[NCPU];
 static struct cpu *bcpu;
 int ismp;
 int ncpu;
-uchar ioapicid;
+uint8 ioapicid;
 
 int mpbcpu(void)
 {
 	return bcpu-cpus;
 }
 
-static uchar sum(uchar *addr, int len)
+static uint8 sum(uint8 *addr, int len)
 {
 	int i, sum;
 	sum = 0;
 	for (i=0; i<len; i++)
 		sum += addr[i];
-	return (uchar) sum;
+	return (uint8) sum;
 }
 
 // Look for an MP structure in the len bytes at addr.
-static struct mp* mpsearch1(uint a, int len)
+static struct mp* mpsearch1(uint32 a, int len)
 {
-	uchar *e, *p, *addr;
-	addr = (uchar *) a;
+	uint8 *e, *p, *addr;
+	addr = (uint8 *) a;
 	e = addr+len;
 	for (p = addr; p < e; p += sizeof(struct mp))
 		if (memcmp(p, "_MP_", 4) == 0 && sum(p, sizeof(struct mp)) == 0)
@@ -151,16 +147,16 @@ static struct mp* mpsearch1(uint a, int len)
 // 3) in the BIOS ROM between 0xE0000 and 0xFFFFF.
 static struct mp* mpsearch(void)
 {
-	uchar *bda;
-	uint p;
+	uint8 *bda;
+	uint32 p;
 	struct mp *mp;
 
-	bda = (uchar *) 0x400;
-	if ((p = (uint)((bda[0x0F]<<8)| bda[0x0E]) << 4)) {
+	bda = (uint8 *) 0x400;
+	if ((p = (uint32)((bda[0x0F]<<8)| bda[0x0E]) << 4)) {
 		if ((mp = mpsearch1(p, 1024)))
 			return mp;
 	} else {
-		p = (uint)((bda[0x14]<<8)|bda[0x13])*1024;
+		p = (uint32)((bda[0x14]<<8)|bda[0x13])*1024;
 		if ((mp = mpsearch1(p-1024, 1024)))
 			return mp;
 	}
@@ -180,12 +176,12 @@ static struct mpconf* mpconfig(struct mp **pmp)
 	if ((mp = mpsearch()) == 0 || mp->physaddr == 0)
 		return 0;
 
-	conf = (struct mpconf*) ((uint) mp->physaddr);
+	conf = (struct mpconf*) ((uint32) mp->physaddr);
 	if (memcmp(conf, "PCMP", 4) != 0)
 		return 0;
 	if (conf->version != 1 && conf->version != 4)
 		return 0;
-	if (sum((uchar*)conf, conf->length) != 0)
+	if (sum((uint8*)conf, conf->length) != 0)
 		return 0;
 	*pmp = mp;
 	return conf;
@@ -193,7 +189,7 @@ static struct mpconf* mpconfig(struct mp **pmp)
 
 void mpinit()
 {
-	uchar *p, *e;
+	uint8 *p, *e;
 	struct mp *mp;
 	struct mpconf *conf;
 	struct mpproc *proc;
@@ -203,8 +199,8 @@ void mpinit()
 	if ((conf = mpconfig(&mp)) == 0)
 		return;
 	ismp = 1;
-	lapic = (uint*)conf->lapicaddr;
-	for (p=(uchar*)(conf+1), e=(uchar*)conf+conf->length; p<e; )
+	lapic = (uint32*)conf->lapicaddr;
+	for (p=(uint8*)(conf+1), e=(uint8*)conf+conf->length; p<e; )
 	{
 		switch (*p) {
 		case MPPROC:
@@ -216,7 +212,7 @@ void mpinit()
 			}
 			if (proc->flags & MPBOOT)
 				bcpu = &cpus[ncpu];
-			cpus[ncpu].id = (uchar) ncpu;
+			cpus[ncpu].id = (uint8) ncpu;
 			ncpu++;
 			p += sizeof(struct mpproc);
 			continue;
